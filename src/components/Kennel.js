d736a78674d37ddfe433f6b6215123560e389fa4
Jason Collum <jasoncollum@gmail.com>
2019-06-01 13:27:28 -0500

    chapter2 lesson

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react'
 import EmployeeList from './employee/EmployeeList'
 import LocationList from './location/LocationList'
+import './Kennel.css'
 
-export default class Kennel extends Component {
+class Kennel extends Component {
+
+    employeesFromAPI = [
+        { id: 1, name: "Jessica Younker" },
+        { id: 2, name: "Jordan Nelson" },
+        { id: 3, name: "Zoe LeBlanc" },
+        { id: 4, name: "Blaise Roberts" }
+    ]
+
+    locationsFromAPI = [
+        { id: 1, name: "Nashville North", address: "500 Puppy Way" },
+        { id: 2, name: "Nashville South", address: "500 Dogbreath Pike" }
+    ]
+
+    state = {
+        employees: this.employeesFromAPI,
+        locations: this.locationsFromAPI
+    }
+
     render() {
         return (
-            <div>
+            <article className="kennel">
                 <h3>Student Kennels</h3>
-                <LocationList />
-                <EmployeeList />
-            </div>
+                <LocationList locations={this.state.locations} />
+                <EmployeeList employees={this.state.employees} />
+            </article>
         );
     }
 }
+
+export default Kennel
